57dcc4b34822a1657b7deea28be1504afaa6983e
JustBSka <89531340993@yandex.ru>
2019-07-11 22:18:24 +0300

    Sorted and removed unused usings.

--- ObjectPool.Tests/FixedPoolTests.cs
+++ ObjectPool.Tests/FixedPoolTests.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Moq;
 
 namespace ObjectPool.Tests
 {
--- ObjectPool/IClearable.cs
+++ ObjectPool/IClearable.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ObjectPool
+﻿namespace ObjectPool
 {
     public interface IClearable
     {
